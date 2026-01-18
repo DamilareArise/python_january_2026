1b090adf9b4b1ff82cb5b25403aade537fae8d78
Arise Damilare <arisedamilare5@gmail.com>
2026-01-18 20:17:37 +0100

    wk1 class2

---
+++ index.py
@@ -0,0 +1,106 @@
+# print(5 + 5)
+
+# commenting
+
+# 1. line comment
+# 2. block comment
+"""
+Hello Techies
+Welcome to Python class 
+This is an example of block comment
+"""
+
+# print("Welcome Mr. Tope Tunji")
+
+
+# # Identation
+
+# print(2+2)
+
+# def run():
+#     print("I can run very fast")
+    
+
+# run()
+    
+
+# Python Variable
+name = "Ojo Ola"
+# variable name, assignment operator and value
+
+# Types of variable declaration
+
+# 1. single variable and single value
+basket = "moimoi"
+# 2. single variable and multiple value
+basket = "moimoi", "eko", "eggs"
+# print(basket)
+# 3. multiple variable and single value
+x = y = z = 10
+y = 20
+# print(x, y, z)
+# 4. Multiple variable and multiple value
+x, y, z = 10, 20, 30
+# print(z)
+
+# Rules 
+# 1. The name must only contain alphabet, number or underscore
+# 2. Variable name don't start with any character aside alpahabet or underscore
+# 3. a variable name does not allow space
+# first_name = 'john' # snake casing
+# firstName = 'john' # camel casing
+# FirstName = "john"  # Pascal casing 
+# 4. Your variable must be descriptive 
+
+
+# Dynamic variable value
+
+# first_name = input("Firstname: ")
+# last_name = input("Lastname: ")
+# print(first_name)
+# print(last_name)
+
+
+# Concatenation
+""    # string
+
+# print("Habeeb " + "Oke")
+# print("Habeeb" + "2")
+
+# print("Habeeb", "Oke")
+# print("habeeb", 2)
+
+
+# name = input("Name: ")
+
+# print("Welcome", name)
+# print("Welcome " + name)
+
+# fullname = "Arise Damilare"
+# course = "Datascience"
+# age = 20
+# account_balance = 2000000
+
+# print("My name is " + fullname + " I am a student of " + course + " I am " + str(age) + "years old")
+
+# print("My name is", fullname, "I am a student of" , course , "I am " , age, "years old")
+
+
+# f - string
+# print(f"My name is {fullname}.  I am a student of {course}. I am {age}years old")
+
+
+# Python Datatypes 
+
+"""
+1. Text type: String  str() e.g "Tayo", 'Ope'
+2. Number type:
+    i. Integer int() e.g 12 , 123, 1234
+    ii. Float float() e.g 12.12
+    iii. Complex complex() e.g 2 + 3j
+
+"""
+
+var1 = "Tayo"
+var = 12.23 + 2j
+print(type(var))
